1d7ed72ecda0141e98bc1f8ad0faff0746397855
maxsmirnov92 <max.smirnov92@yandex.ru>
2024-12-08 14:13:55 +0400

    feature 'notification_reader': cancel package list download when url changed, launch with update state fix when url is empty

--- feature/notification_reader/ui/src/main/java/net/maxsmr/feature/notification_reader/ui/NotificationReaderViewModel.kt
+++ feature/notification_reader/ui/src/main/java/net/maxsmr/feature/notification_reader/ui/NotificationReaderViewModel.kt
@@ -99,7 +99,11 @@ class NotificationReaderViewModel @AssistedInject constructor(
             viewModelScope.launch {
                 _packageListLoadState.postValue(
                     if (loadState != null && !loadState.isSuccessWithData()) {
-                        loadState.copyOf(null)
+                        if (!settings?.packageListUrl.isNullOrEmpty()) {
+                            loadState.copyOf(null)
+                        } else {
+                            LoadState.success(null)
+                        }
                     } else {
                         LoadState.success(
                             PackageListState(
