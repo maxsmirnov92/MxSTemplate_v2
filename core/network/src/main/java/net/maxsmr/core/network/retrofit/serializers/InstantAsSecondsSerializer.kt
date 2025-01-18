4b35dbbd50f9679ad37f83f4a55d6b073bdc9814
maxsmirnov92 <max.smirnov92@yandex.ru>
2025-01-18 18:31:01 +0400

    Instant serializers added

---
+++ core/network/src/main/java/net/maxsmr/core/network/retrofit/serializers/InstantAsSecondsSerializer.kt
@@ -0,0 +1,23 @@
+package net.maxsmr.core.network.retrofit.serializers
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+object InstantAsSecondsSerializer : KSerializer<Instant> {
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("InstantAsLong", PrimitiveKind.LONG)
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        encoder.encodeLong(value.epochSeconds)
+    }
+
+    override fun deserialize(decoder: Decoder): Instant {
+        return Instant.fromEpochSeconds(decoder.decodeLong())
+    }
+}
