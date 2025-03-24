7147aebad9e59a5ce09d85097bb2a86868157953
maxsmirnov92 <max.smirnov92@yandex.ru>
2025-03-24 22:22:07 +0400

    PersistableStateFlow ext for BaseViewModel

--- core/android/src/main/java/net/maxsmr/core/android/base/delegates/Persistable.kt
+++ core/android/src/main/java/net/maxsmr/core/android/base/delegates/Persistable.kt
@@ -2,34 +2,49 @@ package net.maxsmr.core.android.base.delegates
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import kotlinx.coroutines.flow.StateFlow
 import net.maxsmr.core.android.base.BaseViewModel
 import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+fun <T : Any> T.getPersistableKey(kProperty: KProperty<*>): String =
+    "${this::class.simpleName}.${kProperty.name}"
 
-fun <T: Any> T.getPersistableKey(kProperty: KProperty<*>): String =
-        "${this::class.simpleName}.${kProperty.name}"
+fun <T> BaseViewModel.persistableStateFlow(initialValue: T): PersistableStateFlow<T> =
+    PersistableStateFlow(state, initialValue)
 
 fun <T> BaseViewModel.persistableLiveData(): PersistableLiveData<T> = PersistableLiveData(state)
 
 fun <T> BaseViewModel.persistableLiveDataInitial(initialValue: T): PersistableLiveDataInitial<T> =
-        PersistableLiveDataInitial(state, initialValue)
+    PersistableLiveDataInitial(state, initialValue)
 
 fun <T> BaseViewModel.persistableValue(onSetValue: ((T?) -> Unit)? = null): PersistableValue<T> =
-        PersistableValue(state, onSetValue)
+    PersistableValue(state, onSetValue)
 
 fun <T> BaseViewModel.persistableValueInitial(
-        initialValue: T,
-        onSetValue: ((T) -> Unit)? = null,
+    initialValue: T,
+    onSetValue: ((T) -> Unit)? = null,
 ): PersistableValueInitial<T> = PersistableValueInitial(state, initialValue, onSetValue)
 
+/**
+ * Используйте, если нужна [StateFlow], переживающая смерть процесса приложения
+ */
+class PersistableStateFlow<T>(
+    private val state: SavedStateHandle,
+    private val initialValue: T,
+) : ReadOnlyProperty<Any, StateFlow<T>> {
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): StateFlow<T> {
+        return state.getStateFlow(thisRef.getPersistableKey(property), initialValue)
+    }
+}
 
 /**
- * Используйте, если нужна LiveData, переживающая смерть процесса приложения
+ * Используйте, если нужна [MutableLiveData], переживающая смерть процесса приложения
  */
 class PersistableLiveData<T>(
-        private val state: SavedStateHandle
+    private val state: SavedStateHandle,
 ) : ReadOnlyProperty<Any, MutableLiveData<T>> {
 
     override fun getValue(thisRef: Any, property: KProperty<*>): MutableLiveData<T> {
@@ -37,15 +52,14 @@ class PersistableLiveData<T>(
     }
 }
 
-
 /**
  * Используйте, если нужна LiveData, переживающая смерть процесса приложения, с начальным значением.
  * Начальное значение гарантирует, что LiveData всегда содержит какое-либо значение и field.value
  * не вернет null.
  */
 class PersistableLiveDataInitial<T>(
-        private val state: SavedStateHandle,
-        private val initialValue: T,
+    private val state: SavedStateHandle,
+    private val initialValue: T,
 ) : ReadOnlyProperty<Any, MutableLiveData<T>> {
 
     override fun getValue(thisRef: Any, property: KProperty<*>): MutableLiveData<T> {
@@ -60,8 +74,8 @@ class PersistableLiveDataInitial<T>(
  * @param onSetValue доп. действие при смене значения
  */
 class PersistableValue<T>(
-        private val state: SavedStateHandle,
-        private val onSetValue: ((T?) -> Unit)? = null,
+    private val state: SavedStateHandle,
+    private val onSetValue: ((T?) -> Unit)? = null,
 ) : ReadWriteProperty<Any, T?> {
 
     override fun getValue(thisRef: Any, property: KProperty<*>): T? {
@@ -83,9 +97,9 @@ class PersistableValue<T>(
  * @param onSetValue доп. действие при смене значения
  */
 class PersistableValueInitial<T>(
-        private val state: SavedStateHandle,
-        private val initial: T,
-        private val onSetValue: ((T) -> Unit)? = null,
+    private val state: SavedStateHandle,
+    private val initial: T,
+    private val onSetValue: ((T) -> Unit)? = null,
 ) : ReadWriteProperty<Any, T> {
 
     override fun getValue(thisRef: Any, property: KProperty<*>): T {
